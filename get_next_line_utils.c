dc13c0e7cc0a12df8f20729f391af13564018130
Matthew Hadad <mahadad@fu-r7-p3.s19.be>
2021-10-20 16:02:35 +0200

    add len_chrchr();

---
+++ get_next_line_utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 14:20:58 by mahadad           #+#    #+#             */
+/*   Updated: 2021/10/20 15:59:40 by mahadad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/**
+ * @brief `strlen` but whiout crash when `NULL` and stop count when find
+ *         the first occurence of char `c`
+ * 
+ * @param str      the tring to pars
+ * @param c        the caracter to find
+ * @return size_t  the number of character befor `c` (`c` include)
+ */
+size_t	len_chrchr(char *str, char c)
+{
+	char	*start;
+
+	start = str;
+	while (*str && *str != c)
+		str++;
+	if (*str == c)
+		str++;
+	else
+		return (0);
+	return ((size_t)(str - start));
+}
