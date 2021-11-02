a306e932940df54e89b37c704142a992e9aaf0a6
Matthew Hadad <mahadad@fu-r6-p2.s19.be>
2021-11-02 15:32:31 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,23 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 14:02:38 by mahadad           #+#    #+#             */
-/*   Updated: 2021/11/02 15:14:38 by mahadad          ###   ########.fr       */
+/*   Updated: 2021/11/02 15:26:57 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	manage_next_line(t_box *b)
+{
+	b->fill += b->r_ret;
+	b->index = len_chrchr(b->buff, '\n', b->fill);
+	b->tmp = ft_substr(b->buff, 0, b->index + (b->index != b->fill));
+	b->r = strjoin_and_free(&b->r, b->tmp);
+	free(b->tmp);
+	ft_memcpy(b->buff, b->buff + b->index + (b->index != b->fill),
+		b->fill - b->index - (b->index != b->fill));
+	b->fill = b->fill - b->index - (b->index != b->fill);
+}
 
 /**
  * @brief Get the next line object
@@ -28,25 +40,21 @@ char	*get_next_line(int fd)
 	b[fd].r = NULL;
 	while (1)
 	{
-		b[fd].r_ret = read(fd, (b[fd].buff + (b[fd].fill)), BUFFER_SIZE - b[fd].fill);
+		b[fd].r_ret = read(fd, (b[fd].buff + (b[fd].fill)),
+				BUFFER_SIZE - b[fd].fill);
 		if (!b[fd].r_ret && !b[fd].fill)
 		{
 			b[fd].fill = 0;
-			return (b[fd].r);//free buff
+			return (b[fd].r);
 		}
 		if (b[fd].r_ret == -1)
 		{
 			b[fd].fill = 0;
-			return (NULL);//free
+			return (NULL);
 		}
-		b[fd].fill += b[fd].r_ret;
-		b[fd].index = len_chrchr(b[fd].buff, '\n', b[fd].fill);
-		b[fd].tmp = ft_substr(b[fd].buff, 0, b[fd].index + (b[fd].index != b[fd].fill));
-		b[fd].r = strjoin_and_free(&b[fd].r, b[fd].tmp);
-		free(b[fd].tmp);
-		ft_memcpy(b[fd].buff, b[fd].buff + b[fd].index + (b[fd].index != b[fd].fill), b[fd].fill - b[fd].index - (b[fd].index != b[fd].fill));
-		b[fd].fill = b[fd].fill - b[fd].index - (b[fd].index != b[fd].fill);
-		if (b[fd].r && b[fd].r[0] && b[fd].r[strlen_protect(b[fd].r) - 1] == '\n')
+		manage_next_line(&b[fd]);
+		if (b[fd].r && b[fd].r[0]
+			&& b[fd].r[strlen_protect(b[fd].r) - 1] == '\n')
 			break ;
 	}
 	return (b[fd].r);
